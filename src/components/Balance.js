11f46f637d4196a6c3d4ccbd24d8079153608fea
Hasan Shanto <hasanshanto922@gmail.com>
2022-12-30 17:01:47 +0600

    balance calculated

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
     <div>
       <h3>Your Balance:</h3>
-      <h4>$100.00</h4>
+      <h4>${total}</h4>
     </div>
   );
 };
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const Transaction = ({transaction}) => {
-    return (
-      <div>
-        <li>
-          {transaction.text} <span>{transaction.amount}</span>{" "}
-          <button className="delete-btn">x</button>
-        </li>
-      </div>
-    );
+const Transaction = ({ transaction }) => {
+  const sign = transaction.amount > 0 ? "+" : "-";
+  return (
+    <div>
+      <li className={transaction.amount < 0 ? "minus" : "plus"}>
+        {transaction.text}{" "}
+        <span>
+          {sign}${Math.abs(transaction.amount)}
+        </span>{" "}
+        <button className="delete-btn">x</button>
+      </li>
+    </div>
+  );
 };
 
 export default Transaction;
